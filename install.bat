ce9ac4f9a9a426093f43b17530babf38c909f1a0
BarukoTropical <barukoprivat@web.de>
2026-08-09 18:33:39 +0200

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,7 @@
+@echo off
+title SnapchatBoost Installer
+echo Installing dependencies for SnapchatBoost...
+pip install -r requirements.txt
+echo.
+echo Installation complete! You can now run start.bat or main.py.
+pause
