f6940f753433c8a385ed390b530a6d9b31a73560
BarukoTropical <barukoprivat@web.de>
2026-08-09 18:03:09 +0200

    Delete install.bat

--- install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title SnapchatBoost Installer
-echo Installing dependencies for SnapchatBoost...
-pip install -r requirements.txt
-echo.
-echo Installation complete! You can now run start.bat or main.py.
-pause
